fb93cd4d0c8957e855aed1838ec25d50dcb0678f
Steffen Vogel <post@steffenvogel.de>
2021-10-06 04:35:09 +0200

    ev_charging: restructured package

--- pkg/mobility/ev_charging/openchargemap/openchargemap.go
+++ pkg/mobility/ev_charging/openchargemap/openchargemap.go
@@ -1,3 +1,7 @@
 package openchargemap
 
 // https://openchargemap.org/site/develop/api
+
+const (
+	UrlApi = "https://api.openchargemap.io/v3/"
+)
---
+++ pkg/mobility/ev_charging/plugsurfing/plugsurfing.go
@@ -0,0 +1,27 @@
+package plugsurfing
+
+// curl 'https://api.plugsurfing.com/persik/map-charging-stations?&min-lat=52.47831137787191&max-lat=52.56166508540464&min-lon=13.07536415625&max-lon=13.73454384375' \
+//   -H 'authority: api.plugsurfing.com' \
+//   -H 'accept: application/json, text/plain, */*' \
+//   -H 'authorization: 88234shdfsdkl0_$1sdvRd01_233fdd' \
+//   --compressed
+
+//   [
+// 	{
+// 	   "id":1406,
+// 	   "name":"Ladestation Smart innogy SE - eMob",
+// 	   "latitude":52.505073,
+// 	   "longitude":13.343569,
+// 	   "dynamicStatusSummary":"Unknown"
+// 	},
+// 	{
+// 	   "id":1755,
+// 	   "name":"Charging Station innogy SE - eMob",
+// 	   "latitude":52.513015,
+// 	   "longitude":13.329802,
+// 	   "dynamicStatusSummary":"Available"
+// 	},
+
+const (
+	UrlApi = "https://api.plugsurfing.com"
+)
