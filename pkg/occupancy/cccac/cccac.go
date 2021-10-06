567ee825eaf204afc3e33df72a188a700b7a8470
Steffen Vogel <post@steffenvogel.de>
2021-10-06 04:34:45 +0200

    cccac: started to add  docs

--- pkg/occupancy/cccac/cccac.go
+++ pkg/occupancy/cccac/cccac.go
@@ -1,3 +1,11 @@
+/*
+The cccac package fetches the current opening state of club room
+of the Chaos Communication Club in Aachen.
+
+The public API only provides access to a boolean state.
+
+See: https://wiki.aachen.ccc.de/doku.php?id=projekte:clubstatus
+*/
 package cccac
 
 import (
