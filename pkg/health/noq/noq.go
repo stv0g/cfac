2f1addb98877d92a21a7e65ad9ba44c0dcf4c976
Steffen Vogel <post@steffenvogel.de>
2021-11-19 10:40:16 +0100

    started working on no-q.info

---
+++ pkg/health/noq/noq.go
@@ -0,0 +1,8 @@
+package noq
+
+const (
+	// Seems to be some sort of Elasticserach query
+	Url = "https://es-data-proxy.no-q.info/prod/" // + "?source=<src>&source_content_type=application%2Fjson"
+
+	// <src> is URLencoded: {"size":0,"query":{"bool":{"filter":[{"terms":{"tags":["aachen"]}},{"bool":{"should":[{"bool":{"filter":[{"range":{"checkin_at":{"gte":"now","lt":"2021-11-15T23:59:59.999+01:00"}}},{"range":{"min_booking_date":{"lte":"now"}}}]}},{"term":{"placeholder":true}}]}}]}},"aggs":{"gyms":{"terms":{"field":"gym_id","size":10000},"aggs":{"gym":{"top_hits":{"_source":"*","size":1}},"total_free_spots":{"sum":{"field":"free_spots"}},"placeholder":{"terms":{"field":"placeholder"}}}}}}
+)
