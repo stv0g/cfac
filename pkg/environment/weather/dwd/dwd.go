8b6a4c007a5939ce0c217e60f4bd89929cd56ff6
Steffen Vogel <post@steffenvogel.de>
2021-11-30 21:39:48 +0100

    dwd: add some links to API details

--- pkg/environment/weather/dwd/dwd.go
+++ pkg/environment/weather/dwd/dwd.go
@@ -1,3 +1,9 @@
 package dwd
 
 // https://dwd.api.bund.dev/
+// https://listed.to/@DieSieben/7851/api-des-deutschen-wetterdienstes
+// https://www.dwd.de/DE/leistungen/met_verfahren_mosmix/mosmix_stationskatalog.cfg?view=nasPublication&nn=16102
+
+const (
+	UrlApi = "https://app-prod-ws.warnwetter.de/v16/stationOverview?stationIds=10501"
+)
