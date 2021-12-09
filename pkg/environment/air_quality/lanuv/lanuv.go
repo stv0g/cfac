be093281946d3a9bc920a47a894546abfb7e9a63
Steffen Vogel <post@steffenvogel.de>
2021-12-09 20:18:29 +0100

    lanuv: started implementation

--- pkg/environment/air_quality/lanuv/lanuv.go
+++ pkg/environment/air_quality/lanuv/lanuv.go
@@ -1,11 +1,21 @@
 package lanuv
 
-// https://www.lanuv.nrw.de/fileadmin/lanuv/luft/temes/heut/VACW.htm
+import (
+	"bytes"
+	"encoding/csv"
+	"errors"
+	"io"
+	"math"
+	"strconv"
 
-// https://github.com/mpfeil/qualitySCHU
-// https://github.com/mpfeil/lanuv-now
+	"github.com/gocolly/colly/v2"
+	cfac "github.com/stv0g/cfac/pkg"
+)
 
 // https://luadb.lds.nrw.de/LUA/hygon/pegel.php?rohdaten=ja
+// https://www.lanuv.nrw.de/fileadmin/lanuv/luft/temes/heut/VACW.htm
+// https://github.com/mpfeil/qualitySCHU
+// https://github.com/mpfeil/lanuv-now
 // https://github.com/Piccar/NRW-Wetter/blob/master/new1.py
 // https://github.com/smatt/LanuvParser/blob/master/LanuvParser.java
 
@@ -14,3 +24,81 @@ const (
 	UrlCsv       = UrlBase + "/eu_luftqualitaet.csv"
 	UrlCsvHeader = UrlBase + "/header_eu_luftqualitaet.csv"
 )
+
+type Callback func(AirQuality)
+
+func Fetch(c *colly.Collector, cb Callback, ecb cfac.ErrorCallback) {
+	c.OnResponse(func(r *colly.Response) {
+		reader := bytes.NewReader(r.Body)
+		csv_reader := csv.NewReader(reader)
+		csv_reader.Comma = ';'
+
+		for {
+			// Read each record from csv
+			record, err := csv_reader.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				ecb(err)
+			}
+
+			aq, err := ParseCols(record)
+			if err != nil {
+				ecb(err)
+			}
+
+			cb(aq)
+		}
+
+	})
+
+	c.Visit(UrlCsv)
+}
+
+func ParseCols(row []string) (AirQuality, error) {
+	var err error
+
+	if len(row) < 6 {
+		return AirQuality{}, errors.New("invalid number of columns")
+	}
+
+	aq := AirQuality{
+		Station: row[0],
+		ID:      row[1],
+	}
+
+	if aq.Ozon, err = ParseValue(row[2]); err != nil {
+		return aq, err
+	}
+
+	if aq.SO2, err = ParseValue(row[3]); err != nil {
+		return aq, err
+	}
+
+	if aq.NO2, err = ParseValue(row[4]); err != nil {
+		return aq, err
+	}
+
+	if aq.PM10, err = ParseValue(row[5]); err != nil {
+		return aq, err
+	}
+
+	return aq, nil
+}
+
+func ParseValue(s string) (float64, error) {
+	if s == "*" {
+		return math.NaN(), nil
+	}
+
+	if s == "<10" {
+		return math.Inf(-1), nil
+	}
+
+	v, err := strconv.ParseFloat(s, 32)
+	if err != nil {
+		return -1, err
+	}
+
+	return v, nil
+}
---
+++ pkg/environment/air_quality/lanuv/types.go
@@ -0,0 +1,10 @@
+package lanuv
+
+type AirQuality struct {
+	Station string
+	ID      string
+	Ozon    float64
+	SO2     float64
+	NO2     float64
+	PM10    float64
+}
