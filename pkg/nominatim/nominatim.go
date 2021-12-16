89583b0c8a7cf02f95c9c2bdfa148906a9176fe4
Steffen Vogel <post@steffenvogel.de>
2021-12-16 14:29:04 +0100

    nominatim: add simple cache

--- pkg/nominatim/nominatim.go
+++ pkg/nominatim/nominatim.go
@@ -2,7 +2,9 @@ package nominatim
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/url"
+	"sync"
 
 	"github.com/gocolly/colly/v2"
 	cfac "github.com/stv0g/cfac/pkg"
@@ -33,4 +35,37 @@ func Search(c *colly.Collector, query string, cb SearchCallback, ecb cfac.ErrorC
 	q.Add("format", "json")
 
 	c.Visit(UrlSearch + "?" + q.Encode())
+}
+
+var cache = map[string]cfac.Coordinate{}
+
+func SearchAndCache(query string) (cfac.Coordinate, error) {
+	var err error = nil
+	var coord cfac.Coordinate
+
+	if coord, ok := cache[query]; ok {
+		return coord, nil
+	}
+
+	c := colly.NewCollector()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	Search(c, query, func(p []Place) {
+		if len(p) != 1 {
+			err = fmt.Errorf("geo location found %d results", len(p))
+		} else {
+			coord = p[0].Coordinate()
+		}
+
+		wg.Done()
+	}, func(e error) {
+		err = e
+		wg.Done()
+	})
+
+	wg.Wait()
+
+	return coord, err
 }
