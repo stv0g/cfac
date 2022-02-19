d54fa9275715311f1b18954bc92c7e9c839cf3b1
Steffen Vogel <post@steffenvogel.de>
2022-02-19 02:02:24 +0100

    add more measurables to default list

--- pkg/all/all.go
+++ pkg/all/all.go
@@ -1,17 +1,17 @@
 package all
 
 import (
+	_ "github.com/stv0g/cfac/pkg/bsis"
+	_ "github.com/stv0g/cfac/pkg/freifunk"
+	_ "github.com/stv0g/cfac/pkg/health/blutspende"
 	_ "github.com/stv0g/cfac/pkg/mobility/apag"
 	_ "github.com/stv0g/cfac/pkg/occupancy/carolus"
 	_ "github.com/stv0g/cfac/pkg/occupancy/cccac"
-	_ "github.com/stv0g/cfac/pkg/occupancy/terminmanagement"
-
-	_ "github.com/stv0g/cfac/pkg/freifunk"
-	_ "github.com/stv0g/cfac/pkg/health/blutspende"
 	_ "github.com/stv0g/cfac/pkg/occupancy/gyms/fitx"
 	_ "github.com/stv0g/cfac/pkg/occupancy/gyms/mcfit"
 	_ "github.com/stv0g/cfac/pkg/occupancy/gyms/medaix"
 	_ "github.com/stv0g/cfac/pkg/occupancy/gyms/wof"
+	_ "github.com/stv0g/cfac/pkg/occupancy/terminmanagement"
 	// _ "github.com/stv0g/cfac/pkg/occupancy/gyms/hochschulsport"
 	// _ "github.com/stv0g/cfac/pkg/occupancy/spielbank"
 )
